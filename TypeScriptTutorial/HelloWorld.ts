b67d8a01d2a6e07c1752cd864f84b782eebd9e7e
Yimin <yymone84@hotmail.com>
2021-05-15 19:26:07 +1000

    Add

---
+++ TypeScriptTutorial/HelloWorld.ts
@@ -0,0 +1,10 @@
+var message:string  = "Hello world."
+console.log(message);
+
+class Greeting { 
+    greet():void { 
+       console.log("Hello World!!!") 
+    } 
+ } 
+ var obj = new Greeting(); 
+ obj.greet();
